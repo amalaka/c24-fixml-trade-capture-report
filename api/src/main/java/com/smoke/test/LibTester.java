46748f51de539d3a13e039203fe5a3a8ed904b06
Matt Vickery <matt.d.vickery@gmail.com>
2013-04-03 12:34:32 +0100

    #9 general project updates.

---
+++ api/src/main/java/com/smoke/test/LibTester.java
@@ -0,0 +1,12 @@
+package com.smoke.test;
+
+/**
+ * Created on behalf of C24 Technologies Ltd.
+ *
+ * @author mvickery
+ * @since 03/04/2013
+ */
+public class LibTester {
+    
+    //private FillElement fe = FillElement.getInstance();
+}
