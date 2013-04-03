ca73471cbdb70e072cfc43bebd2f6eb539c47f94
Matt Vickery <matt.d.vickery@gmail.com>
2013-04-03 13:35:06 +0100

    #9 general project updates.

---
+++ c24-fixml-api/src/main/java/biz/c24/io/fixml/sample/configuration/C24iOConfiguration.java
@@ -0,0 +1,13 @@
+package biz.c24.io.fixml.sample.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created on behalf of C24 Technologies Ltd.
+ *
+ * @author mvickery
+ * @since 03/04/2013
+ */
+@Configuration
+public class C24iOConfiguration {
+}
