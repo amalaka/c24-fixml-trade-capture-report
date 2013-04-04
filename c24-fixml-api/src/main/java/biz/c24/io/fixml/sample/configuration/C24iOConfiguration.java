783fa253dc0c3a0e68f50f1a0c87f737daff522c
Matt Vickery <matt.d.vickery@gmail.com>
2013-04-04 10:49:38 +0100

    #9 spring contexts setup.

--- c24-fixml-api/src/main/java/biz/c24/io/fixml/sample/configuration/C24iOConfiguration.java
+++ c24-fixml-api/src/main/java/biz/c24/io/fixml/sample/configuration/C24iOConfiguration.java
@@ -1,6 +1,9 @@
 package biz.c24.io.fixml.sample.configuration;
 
+import biz.c24.io.api.data.Element;
 import biz.c24.io.fixml.sample.storage.MongoDbWriter;
+import biz.c24.io.spring.core.C24Model;
+import biz.c24.io.spring.source.TextualSourceFactory;
 import com.smoke.test.ConsolePrinter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,14 +18,33 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource(value = "classpath:META-INF/spring/flow-config.xml")
 @Configuration
 public class C24iOConfiguration {
- 
-    @Bean (name = "debug")
+
+    @Bean(name = "debug")
     public ConsolePrinter getConsolePrinter() {
+
         return new ConsolePrinter();
     }
-    
-    @Bean (name = "mongoDBWriter")
+
+    @Bean(name = "mongoDBWriter")
     public MongoDbWriter getMongoDbWriter() {
+
         return new MongoDbWriter("myServer", "1521");
-    } 
+    }
+
+    @Bean(name = "textualSourceFactory")
+    public TextualSourceFactory getTextualSourceFactory() {
+
+        TextualSourceFactory textualSourceFactory = new TextualSourceFactory();
+        textualSourceFactory.setEncoding("UTF-8");
+
+        return textualSourceFactory;
+    }
+
+    @Bean(name = "fixmlModel")
+    public C24Model getFixmlModel() {
+
+        // TODO: get the correct element name for the FIXML element.
+        Element element = new Element();
+        return new C24Model(element);
+    }
 }
