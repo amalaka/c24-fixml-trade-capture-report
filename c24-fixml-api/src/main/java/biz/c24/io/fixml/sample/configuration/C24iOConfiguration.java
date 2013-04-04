5fd643fe356dd693fc73d46ec373fba3dcfb7bf0
Matt Vickery <matt.d.vickery@gmail.com>
2013-04-04 11:55:38 +0100

    #9 spring contexts setup.

--- c24-fixml-api/src/main/java/biz/c24/io/fixml/sample/configuration/C24iOConfiguration.java
+++ c24-fixml-api/src/main/java/biz/c24/io/fixml/sample/configuration/C24iOConfiguration.java
@@ -1,13 +1,18 @@
 package biz.c24.io.fixml.sample.configuration;
 
 import biz.c24.io.api.data.Element;
+import biz.c24.io.api.presentation.JsonSink;
 import biz.c24.io.fixml.sample.storage.MongoDbWriter;
 import biz.c24.io.spring.core.C24Model;
 import biz.c24.io.spring.source.TextualSourceFactory;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
 import com.smoke.test.ConsolePrinter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+
+import java.net.UnknownHostException;
 
 /**
  * Created on behalf of C24 Technologies Ltd.
@@ -19,32 +24,49 @@ import org.springframework.context.annotation.ImportResource;
 @Configuration
 public class C24iOConfiguration {
 
+    private final String mongoServer = "localhost";
+    private final int mongoPort = 1723;
+    private final String mongoDBName = "fixmlDB";
+    private final String mongoCollectionName = "messages";
+
     @Bean(name = "debug")
     public ConsolePrinter getConsolePrinter() {
-
         return new ConsolePrinter();
     }
 
-    @Bean(name = "mongoDBWriter")
-    public MongoDbWriter getMongoDbWriter() {
-
-        return new MongoDbWriter("myServer", "1521");
+    @Bean(name = "mongoDBFIXMLCollectionWriter")
+    public MongoDbWriter getMongoDbWriter() throws UnknownHostException {
+        MongoDbWriter mongoDbWriter = new MongoDbWriter(getFIXMLCollection(), getJsonSink());
+        return mongoDbWriter;
     }
 
     @Bean(name = "textualSourceFactory")
     public TextualSourceFactory getTextualSourceFactory() {
-
         TextualSourceFactory textualSourceFactory = new TextualSourceFactory();
         textualSourceFactory.setEncoding("UTF-8");
-
         return textualSourceFactory;
     }
 
     @Bean(name = "fixmlModel")
     public C24Model getFixmlModel() {
-
         // TODO: get the correct element name for the FIXML element.
         Element element = new Element();
         return new C24Model(element);
+    }
+
+    @Bean
+    public MongoClient getMongoClient() throws UnknownHostException {
+        return new MongoClient(mongoServer, mongoPort);
+    }
+
+    @Bean
+    public JsonSink getJsonSink() {
+        return new JsonSink();
+    }
+
+    @Bean(name = "fixmlCollection")
+    public DBCollection getFIXMLCollection() throws UnknownHostException {
+        MongoClient mongoClient = getMongoClient();
+        return  mongoClient.getDB(mongoDBName).getCollection(mongoCollectionName);
     }
 }
--- c24-fixml-api/src/main/java/biz/c24/io/fixml/sample/storage/MongoDbWriter.java
+++ c24-fixml-api/src/main/java/biz/c24/io/fixml/sample/storage/MongoDbWriter.java
@@ -1,24 +1,42 @@
 package biz.c24.io.fixml.sample.storage;
 
 import biz.c24.io.api.data.ComplexDataObject;
+import biz.c24.io.api.presentation.JsonSink;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.util.JSON;
 import org.springframework.integration.Message;
 import org.springframework.integration.annotation.ServiceActivator;
 
+import java.io.IOException;
+import java.io.StringWriter;
+
 public class MongoDbWriter {
 
-    public MongoDbWriter(final String serverName, String port) {
+    private JsonSink sink;
+    private DBCollection mongoDBCollection;
+
+    public MongoDbWriter(final DBCollection mongoDBCollection, final JsonSink sink) {
+        this.mongoDBCollection = mongoDBCollection;
+        this.sink = sink;
     }
-    
+
     @ServiceActivator
     public void store(final Message<?> message) {
-       
         System.out.println("Storing...");
         if (message.getPayload() instanceof ComplexDataObject) {
-            // store
-        }
-        else {
+            ComplexDataObject cdo = (ComplexDataObject) message.getPayload();
+            try {
+                StringWriter writer = new StringWriter();
+                sink.setWriter(writer);
+                sink.writeObject(cdo);
+                BasicDBObject obj = (BasicDBObject) JSON.parse(writer.toString());
+                mongoDBCollection.save(obj);
+            } catch (IOException e) {
+                System.out.println("An exception occurred while writing the cdo to the MongoDB database. " + e);
+            }
+        } else {
             throw new RuntimeException("buggered...");
         }
-        
     }
 }
